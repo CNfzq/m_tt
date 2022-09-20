9d9bb408785a51816d6585e15ad1b5595dca187d
fangzhiqiang <15079480482@163.com>
2022-09-20 11:25:23 +0800

    项目初始化--引入vant组件库

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
   <div id="app">
     <h1>今日头条</h1>
+    <i class="toutiao toutiao-yanzhengma"></i>
+    <van-button loading type="primary" />
+    <van-button loading type="primary" loading-type="spinner" />
+    <van-button loading type="info" loading-text="加载中..." />
     <router-view />
+    <van-cell-group inset>
+      <van-cell title="单元格" value="内容" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+    </van-cell-group>
   </div>
 </template>
-<script lang="ts">
+
+<script>
 export default {
   // 给每个组件取个名字
   name: "App",
--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
+import Vant from "vant";
+
 import router from "./router";
 import store from "./store";
 
 import "@/styles/index.less";
+import "vant/lib/index.css";
 
+Vue.use(Vant);
 Vue.config.productionTip = false;
 
 new Vue({
